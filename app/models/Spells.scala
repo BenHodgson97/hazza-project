3ca27607963bd41b0ec48716573ae0d245cf3d10
BenHodgson97 <48325296+BenHodgson97@users.noreply.github.com>
2021-06-24 19:41:41 +0100

    Added Database Connection (#4)
    Co-authored-by: Dane Watts <danewatts97@gmail.com>

--- app/models/Spells.scala
+++ app/models/Spells.scala
@@ -1,14 +1,23 @@
 package models
 
+import play.api.libs.json.{Json, Reads}
+
 case class Spells(
                    id: Int,
                    name: String,
                    rank: Int,
-                   spellType: String, //TODO Enumerated
-                   skill: String, //TODO Enumerated
+                   opposing: Boolean,
+                   spellType: Seq[String], //TODO Enumerated
+                   skill: Seq[String], //TODO Enumerated
                    abilityType: String, //TODO Enumerated
                    cost: Int,
                    description: String,
                    group: Int,
                    treePosition: Int
                  )
+
+
+
+object Spells {
+  implicit val spellsReads: Reads[Spells] = Json.reads[Spells]
+}
