df8a4e12180ebb503a8d30583ab8b0770eeb5ac1
Dane Watts <danewatts97@gmail.com>
2021-10-11 18:47:13 +0100

    more heroku firefighting

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -1,5 +1,8 @@
 package config
 
-class AppConfig {
+import com.google.inject.Inject
+import play.api.Configuration
 
+class AppConfig @Inject()(configuration: Configuration){
+  val isSecure: Boolean = configuration.get[Boolean]("isSecure")
 }
