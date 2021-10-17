04520875379ecf84e77b28fa74f0314945c8aee8
Dane Watts <danewatts97@gmail.com>
2021-10-17 18:04:14 +0100

    created stylesheet for skillTree and getAbilityByGroup function

--- app/controllers/SkillTreeController.scala
+++ app/controllers/SkillTreeController.scala
@@ -2,8 +2,10 @@ package controllers
 
 import actions.AuthAction
 import com.google.inject.{Inject, Singleton}
+import models.ability.Group.Light
 import models.request.AuthenticatedRequest
 import play.api.mvc._
+import repositories.AbilityRepository
 import views.html.SkillTreeView
 
 import scala.concurrent.ExecutionContext
@@ -12,10 +14,14 @@ import scala.concurrent.ExecutionContext
 class SkillTreeController @Inject()(
                                    val controllerComponents: ControllerComponents,
                                    skillTreeView: SkillTreeView,
+                                   abilityRepository: AbilityRepository,
                                    authAction: AuthAction
                                    )(implicit executionContext: ExecutionContext) extends BaseController {
 
-  def onPageLoad: Action[AnyContent] = authAction {implicit request: AuthenticatedRequest[AnyContent] =>
-    Ok(skillTreeView())
+  def onPageLoad: Action[AnyContent] = authAction.async {implicit request: AuthenticatedRequest[AnyContent] =>
+
+    abilityRepository.getAbilityByGroup(Light).map {
+      abilities => Ok(skillTreeView(abilities))
+    }
   }
 }
