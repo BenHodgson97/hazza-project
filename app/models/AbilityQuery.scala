45b131717925c5433e8a2996e6bfb38eb8c0c024
Dane Watts <danewatts97@gmail.com>
2021-12-06 17:34:31 +0000

    created first filter (group) for ability list

--- app/models/AbilityQuery.scala
+++ app/models/AbilityQuery.scala
@@ -1,5 +1,7 @@
 package models
 
-case class AbilityQuery(search: String) {
-  val searchToRegex: String = "^.*" + search.replace(" ", ".*") + ".*$"
+import models.ability.Group
+
+case class AbilityQuery(search: Option[String], groupFilter: Option[Seq[Group]]) {
+  val searchToRegex: Option[String] = search.map(search => "^.*" + search.replace(" ", ".*") + ".*$")
 }
